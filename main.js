4207dcebe689901b3edda1333c72769c25cb6f5e
engineergirly <girlytambeagbor@gmail.com>
2024-03-04 23:51:21 -0800

    building out logic for winning patterns

--- main.js
+++ main.js
@@ -14,3 +14,21 @@ box.forEach(cell => {
         turn == 'X' ? turn = 'O' : turn = 'X'
     }
 })
+
+function winner () {
+    const winCombination = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    winCombination.forEach(element => {
+        // if ()
+    })
+}
+
